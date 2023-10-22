e27c4a314b0046bfc720d338e5f1a7b00e389baf
melikatfi <87155734+melikatfi@users.noreply.github.com>
2023-10-22 17:56:30 +0330

    initial files added

---
+++ resources/js/Pages/Posts/PostComponent.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+import Navbar from '../../Components/Navbar';
+
+export default function PostComponent() {
+    return (
+        <>
+        <Navbar />
+        <div>hello</div>
+        </>
+    );
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -25,6 +26,8 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/posts',[PostController::class, 'index']);
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -34,5 +37,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+
 
 require __DIR__.'/auth.php';
